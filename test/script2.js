0d186c791ce60c34d0a78c43819dbb38f2eaa45d
Қалдыбай Алихан <Qaldybay_A06@krg.nis.edu.kz>
2026-09-10 16:57:01 +0500

    Added bar

--- test/script2.js
+++ test/script2.js
@@ -7,3 +7,24 @@ if (best){
 else{
     bestScore.textContent="N/A";
 }
+const d=document.getElementById("box");
+const dd=document.getElementById("up_box");
+let width=null;
+window.addEventListener("load",()=>{
+    dd.style.top=d.offsetTop+"px";
+    dd.style.left=d.offsetLeft+"px";
+    const rect=d.getBoundingClientRect();
+    width=rect.width;
+    requestAnimationFrame(loop);
+});
+var last=0;
+var dt=0;
+function loop(time){
+    dt=(time-last)/1000.0;
+    last=time;
+    if (50*dt>width-dd.offsetWidth){
+        dd.style.width=width+"px";
+    }
+    dd.style.width=Number(dd.style.width.slice(0,-2))+50*dt+"px";
+    requestAnimationFrame(loop);
+}
